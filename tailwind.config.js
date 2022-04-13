e4a0ce14f1c6568d9b19602d7216e157dc7479a5
DanyalBani <mr.d.bani.d@gmail.com>
2022-04-14 03:48:07 +0430

    adding max-width size of 16

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
     content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            maxWidth: {
+                16: "16rem",
+            },
+        },
     },
     plugins: [],
 };
